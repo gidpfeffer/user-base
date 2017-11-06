bba73c2accd5d93212fdc9163670a0112a25ef22
Gideon Pfeffer <gid.pfeffer@gmail.com>
2017-11-06 23:19:42 +0000

    added devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    
+    if user_signed_in?
+      redirect_to '/welcome/welcome'
+    else
+      redirect_to new_user_session_path
+    end
+    
   end
+  
+  def welcome
+  end
+  
 end
